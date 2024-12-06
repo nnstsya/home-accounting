872903765da29b77527c9e2b9d88accd6806ac7d
nnstsya <tonastyaket@gmail.com>
2024-12-06 16:31:31 +0200

    feat(HA-7.3) Added pagination, sorting and filter
    - implemented pagination, sorting and filter by all columns of table, added button under the table

--- src/app/home/history/history-table/history-table.component.ts
+++ src/app/home/history/history-table/history-table.component.ts
@@ -1,26 +1,51 @@
-import { Component, input, InputSignal, OnInit } from '@angular/core';
+import { AfterViewInit, Component, input, InputSignal, Signal, viewChild } from '@angular/core';
 import { MatTableDataSource } from '@angular/material/table';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
 import { ExtendedEventModel } from '@home/models/event.model';
 
-type IndexedData = ExtendedEventModel & { index: number };
+interface IndexedData extends Omit<ExtendedEventModel, 'category'> {
+  index: number;
+  category: string;
+}
 
 @Component({
   selector: 'app-history-table',
   templateUrl: './history-table.component.html',
-  styleUrl: 'history-table.component.scss'
+  styleUrls: ['./history-table.component.scss'],
 })
-export class HistoryTableComponent implements OnInit {
+export class HistoryTableComponent implements AfterViewInit {
   data: InputSignal<ExtendedEventModel[]> = input.required<ExtendedEventModel[]>();
 
   displayedColumns: string[] = ['index', 'amount', 'date', 'category', 'type', 'actions'];
-  dataSource: MatTableDataSource<IndexedData> = new MatTableDataSource();
+  dataSource: MatTableDataSource<IndexedData>;
 
-  ngOnInit() {
+  paginator: Signal<MatPaginator | undefined> = viewChild<MatPaginator>(MatPaginator);
+  sort: Signal<MatSort> = viewChild.required<MatSort>(MatSort);
+
+  constructor() {
+    this.dataSource = new MatTableDataSource();
+  }
+
+  ngAfterViewInit(): void {
     const dataWithIndex: IndexedData[] = this.data().map((item, index) => ({
       ...item,
+      category: item.category.name,
       index: index + 1,
     }));
 
     this.dataSource = new MatTableDataSource(dataWithIndex);
+
+    this.dataSource.paginator = this.paginator() ?? null;
+    this.dataSource.sort = this.sort();
+  }
+
+  applyFilter(event: Event) {
+    const filterValue: string = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
   }
 }
