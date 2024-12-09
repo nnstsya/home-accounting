39e5a7e8a2425f091f9c0d58f5c5d6477274ce4a
nnstsya <tonastyaket@gmail.com>
2024-12-09 15:12:09 +0200

    feat(HA-8.2) implemented add event button
    - implemented add event button, fixed bug with event details page

--- src/app/home/records/records.component.ts
+++ src/app/home/records/records.component.ts
@@ -2,6 +2,8 @@ import { Component, inject, OnInit } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { EventCategoryModel } from '@home/models/event.model';
 import { AccountingService } from '@home/services/accounting.service';
+import { MatDialog } from '@angular/material/dialog';
+import { AddEventModalComponent } from '@home/modals/add-event-modal/add-event-modal.component';
 
 @Component({
   selector: 'app-records',
@@ -14,8 +16,17 @@ export class RecordsComponent implements OnInit {
   userId: string = JSON.parse(localStorage.getItem('user')!).id;
 
   private accountingService: AccountingService = inject(AccountingService);
+  private dialog: MatDialog = inject(MatDialog);
 
   ngOnInit() {
     this.userCategories$ = this.accountingService.getCurrentUserCategories(this.userId);
+  }
+
+  openAddEventModal(): void {
+    this.dialog.open(AddEventModalComponent, {
+      width: '400px',
+      disableClose: true,
+      data: { categories: this.userCategories$ }
+    });
   }
 }
